5cb0b5d52859c537358cf80193065ac83e078427
Vladimir Kalinin <Vofamchic@mail.ru>
2024-04-09 21:26:25 +0300

    app veyor

--- src/js/__tests__/validators.test.js
+++ src/js/__tests__/validators.test.js
@@ -1,16 +1,14 @@
-import {valid_credit_card} from '../luhnValidator';
+import { valid_credit_card } from '../luhnValidator';
 
 test('should be false', () => {
-    const result = valid_credit_card('123');
+  const result = valid_credit_card('123');
 
-    expect(result).toBe(false);
+  expect(result).toBe(false);
 });
 
 
-
-
 test('should be true', () => {
-    const result = valid_credit_card('5482845241578578');
+  const result = valid_credit_card('5482845241578578');
 
-    expect(result).toBe(true);
+  expect(result).toBe(true);
 });
